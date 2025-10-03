b3fa2a3f7a6f97fe63bcbbcc6125d6325cfc474a
Robert Nguyen <tuananhnguyen@Tuans-Mac-mini.local>
2025-10-03 13:03:03 +0700

    feat: initial commit from Create Next App

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+<<<<<<< HEAD
   output: "standalone",
+=======
+  /* config options here */
+>>>>>>> 32d98d4 (Initial commit from Create Next App)
 };
 
 export default nextConfig;
