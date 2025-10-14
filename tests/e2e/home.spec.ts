47b858fc2b8ec07152cfec08d76081be6f206de9
Robert Nguyen <tuananhnguyen@Tuans-Mac-mini.local>
2025-10-14 20:16:32 +0700

    fix(ci): test only run 3001

--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -2,9 +2,13 @@ import { expect, test } from "@playwright/test";
 
 test("renders home with gretting", async ({ page }) => {
 <<<<<<< HEAD
+<<<<<<< HEAD
   await page.goto("http://localhost:3001");
 =======
   await page.goto("http://localhost:3000");
 >>>>>>> 8a18953 (feat: create sample data)
+=======
+  await page.goto("http://localhost:3001");
+>>>>>>> f1ac84b (fix(ci): test only run 3001)
   await expect(page.locator("p")).toContainText("Hello, Developer");
 });
